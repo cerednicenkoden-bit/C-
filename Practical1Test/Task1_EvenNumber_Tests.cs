3b73e477316bb3fdcfa20e71f0e1275473e41556
cerednicenkoden-bit <cerednicenkoden@gmail.com>
2025-11-06 16:21:53 +0200

    Add unit tests for even and odd number checks

---
+++ Practical1Test/Task1_EvenNumber_Tests.cs
@@ -0,0 +1,48 @@
+namespace Practical1Test
+{
+    public class Task1_EvenNumber_Tests
+    {
+
+        [Test]
+        [TestCase(2, true)]
+        [TestCase(4, true)]
+        [TestCase(0, true)]
+        [TestCase(-2, true)]
+        public void IsEven_EvenNumbers_ReturnsTrue(int number, bool expected)
+        {
+            bool result = Task1.Program.IsEven(number);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(1, false)]
+        [TestCase(3, false)]
+        [TestCase(-1, false)]
+        [TestCase(-3, false)]
+        public void IsEven_OddNumbers_ReturnsFalse(int number, bool expected)
+        {
+            bool result = Task1.Program.IsEven(number);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(2, "Двері відкриваються!")]
+        [TestCase(4, "Двері відкриваються!")]
+        [TestCase(0, "Двері відкриваються!")]
+        public void GetMessage_EvenNumbers_ReturnsOpenMessage(int number, string expected)
+        {
+            string result = Task1.Program.GetMessage(number);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(1, "Двері зачинені...")]
+        [TestCase(3, "Двері зачинені...")]
+        [TestCase(-1, "Двері зачинені...")]
+        public void GetMessage_OddNumbers_ReturnsClosedMessage(int number, string expected)
+        {
+            string result = Task1.Program.GetMessage(number);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
