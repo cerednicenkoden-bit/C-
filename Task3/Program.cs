3fa5e399c0786793716c79d6d30f62d7a4ab1e6b
cerednicenkoden-bit <cerednicenkoden@gmail.com>
2025-11-06 16:05:03 +0200

    Update Program.cs

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,43 @@
+﻿namespace Task3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть ваш вік:");
+            if (int.TryParse(Console.ReadLine(), out int age))
+            {
+                string classification = ClassifyAge(age);
+                Console.WriteLine(classification);
+            }
+            else
+            {
+                Console.WriteLine("Будь ласка, введіть дійсний вік.");
+            }
+        }
 
+        public static string ClassifyAge(int age)
+        {
+            if (age < 0 || age > 120)
+            {
+                return "Нереальний вік";
+            }
+            else if (age <= 11)
+            {
+                return "Ви дитина";
+            }
+            else if (age <= 17)
+            {
+                return "Підліток";
+            }
+            else if (age <= 59)
+            {
+                return "Дорослий";
+            }
+            else
+            {
+                return "Пенсіонер";
+            }
+        }
+    }
+}
