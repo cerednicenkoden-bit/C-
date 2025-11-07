e5ed357b9b72083a3b08c7e736c434d9770b0168
cerednicenkoden-bit <cerednicenkoden@gmail.com>
2025-11-07 17:25:39 +0200

    Add main program for Hospital Management System

---
+++ HospitalManagementSystem/Program.cs
@@ -0,0 +1,9 @@
+﻿using HospitalManagementSystem;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+HospitalDemo demo = new HospitalDemo();
+demo.Run();
+
+Console.WriteLine("\nНатисніть будь-яку клавішу для виходу...");
+Console.ReadKey();
